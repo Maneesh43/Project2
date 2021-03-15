611d8883df0a998afe6fda78fd91429c495c16fe
Maneesh43 <thouti.maneesh43@gmail.com>
2021-03-14 20:09:07 -0700

    added logo and removed heading

--- scripts/vaccineregister.js
+++ scripts/vaccineregister.js
@@ -1,30 +1,30 @@
 
 
-// document.querySelector('.buttonregister').addEventListener('click',()=>{
+document.querySelector('.buttonregister').addEventListener('click',()=>{
 
-// var db = firebase.firestore();
+var db = firebase.firestore();
 
 
-// let user=document.querySelector('#username').value;
-// let age=document.querySelector('#age').value;
-// console.log(user);
+let user=document.querySelector('#username').value;
+let age=document.querySelector('#age').value;
+console.log(user);
 
-// let priorityvalue=prioritycalc();
+let priorityvalue=prioritycalc();
 
-// db.collection("userdata").doc(user).set({
-// name: user,
-// age:age,
-// priority:priorityvalue,
-// },{merge:true})
-// .then(() => {
-// console.log("Document successfully written!");
-// alert("success")
-// })
-// .catch((error) => {
-// console.error("Error writing document: ", error);
-// });
+db.collection("userdata").doc(user).set({
+name: user,
+age:age,
+priority:priorityvalue,
+},{merge:true})
+.then(() => {
+console.log("Document successfully written!");
+alert("success")
+})
+.catch((error) => {
+console.error("Error writing document: ", error);
+});
 
-// })
-// function prioritycalc(){
-//     return "345";
-// }
+})
+function prioritycalc(){
+    return "345";
+}
