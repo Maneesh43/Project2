b34c0c5f4eef5bd8b3875496f367517f9736720a
Maneesh43 <thouti.maneesh43@gmail.com>
2021-03-15 17:15:09 -0700

    created new vaccine_registration page

--- README.md
+++ README.md
@@ -3,7 +3,4 @@ Team Project for Term 2
 Project:Vaccine Rollout Planner
 Project Name:Lifeline
 
-access it here
-maneesh43.github.io/Project2
-
 
--- scripts/vaccineregister.js
+++ scripts/vaccineregister.js
@@ -1,30 +1,67 @@
+// Geolocation api
+let locatevalue=document.querySelector("#locationtext");
+document.querySelector('#locateme').addEventListener('click',getLocation);
 
+function getLocation(){
+    function success(position) {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(latitude);
+        console.log(longitude);
+        locatevalue.value="lat:"+latitude+"lon"+longitude;
+    
+        // status.textContent = '';
+        // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        // mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+      }
+    
+      function error() {
+          alert("You have denied access to location!");
 
-document.querySelector('.buttonregister').addEventListener('click',()=>{
+      }
+    
+      if(!navigator.geolocation) {
+          alert("Geolocation API not supported");
+      } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+      }
+    
+    }
 
-var db = firebase.firestore();
-
-
-let user=document.querySelector('#username').value;
-let age=document.querySelector('#age').value;
-console.log(user);
-
-let priorityvalue=prioritycalc();
-
-db.collection("userdata").doc(user).set({
-name: user,
-age:age,
-priority:priorityvalue,
-},{merge:true})
-.then(() => {
-console.log("Document successfully written!");
-alert("success")
+document.querySelector('#chckbox').addEventListener('checked',()=>{
+    alert("checked");
 })
-.catch((error) => {
-console.error("Error writing document: ", error);
-});
 
-})
-function prioritycalc(){
-    return "345";
-}
+
+
+
+
+
+// document.querySelector('.buttonregister').addEventListener('click',()=>{
+
+// var db = firebase.firestore();
+
+
+// let user=document.querySelector('#username').value;
+// let age=document.querySelector('#age').value;
+// console.log(user);
+
+// let priorityvalue=prioritycalc();
+
+// db.collection("userdata").doc(user).set({
+// name: user,
+// age:age,
+// priority:priorityvalue,
+// },{merge:true})
+// .then(() => {
+// console.log("Document successfully written!");
+// alert("success")
+// })
+// .catch((error) => {
+// console.error("Error writing document: ", error);
+// });
+
+// })
+// function prioritycalc(){
+//     return "something for now"
+// }
