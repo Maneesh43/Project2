97546f3068ebdf893327631f21ed48ea20991d8f
Maneesh43 <thouti.maneesh43@gmail.com>
2021-03-24 20:07:33 -0700

    validations

---
+++ scripts/validation.js
@@ -0,0 +1,17 @@
+
+
+
+function is_email(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function is_empty(lengthy){
+
+    if((lengthy.length<=0)){
+        return false;
+        console.log("Function error");
+    }
+
+}
+
