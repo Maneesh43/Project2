c3a3fbd2e238428d76f7c09fc664c0ae3bb8c3d1
Maneesh43 <thouti.maneesh43@gmail.com>
2021-03-28 21:07:29 -0700

    vaccineupdtes page updated

--- scripts/updates.js
+++ scripts/updates.js
@@ -1,6 +1,6 @@
 // Uses diseases.sh open api for all the data presented here.
-// Updated every 10 minutes.
-dropdownfunc();
+// Covid 19 statistics from worldometer and is Updated every 10 minutes.
+
 
 // Async function 
 async function fetchdata(a){
@@ -14,6 +14,7 @@ async function fetchdata(a){
 // set interval 10 minutes
 gettodaysdata();
 vaccine_coverage();
+vaccinecandidates();
 setInterval(()=>{
     gettodaysdata();
 },600000)
@@ -23,20 +24,23 @@ function gettodaysdata(){
 // getting todays cases
     let a=fetchdata("https://disease.sh/v3/covid-19/all").then(function(value){
     console.log(value);
-    document.querySelector(".cardstoday").innerHTML=`<p>Total covid-19 cases till date: ${(value.cases).toLocaleString()} </p>
-    <p>Cases Today: ${value.todayCases}</p>
-    <p>Cases Active: ${value.active}</p>
-    <p>People Recovered: ${value.recovered}</p>
-    <p>People recovered today: ${value.todayRecovered}</p>
-    <p>Total number of tests till date: ${value.tests}</p>`
+    document.querySelector(".cardstoday").innerHTML=`<table><caption>Covid-19 Statistics</caption><tr><td>Total covid-19 cases till date:</td><td> ${(value.cases).toLocaleString()} </td></tr>
+    <tr><td>Cases Today:</td> <td>${value.todayCases}</td></tr>
+    <tr><td>Cases Active: </td><td>${value.active}</td></tr>
+    <tr><td>People Recovered:</td> <td>${value.recovered}</td></tr>
+    <tr><td>People recovered today:</td> <td>${value.todayRecovered}</td></tr>
+    <tr><td>Total number of tests till date:</td> <td>${value.tests}</td></tr>
+    </table>`
 })
 }
+// Vaccine Related info is from from RAPS (Regulatory Affairs Professional Society). Specifically published by Jeff Craven at https://www.raps.org/news-and-articles/news-articles/2020/3/covid-19-vaccine-tracker and is updated every 24hrs.
 
 function vaccine_coverage(){
     let select =document.querySelector("#selectcountry");
     let vaccinecontainer=document.querySelector(".cardsvaccine");
     let url ="https://disease.sh/v3/covid-19/vaccine/coverage/countries?lastdays=1"
     let a=fetchdata(url).then(function(value){
+        // console.log(value);
         value.forEach(element => {
         let option=document.createElement("option");
         option.text=element.country;
@@ -59,6 +63,42 @@ function vaccine_coverage(){
     })
 }
 
-// Drop down function
+// Vaccine Candidates
+function vaccinecandidates(){
+    let container=document.querySelector(".vaccinedata");
+    let select=document.querySelector("#vaccinecandidate");
+    url="https://disease.sh/v3/covid-19/vaccine"
+    let arrayvaccine=[];
+    let a=fetchdata(url).then(function(value){
+        // console.log(typeof(value.data));
+        let arrayvaccine=value.data;
+        arrayvaccine.forEach(item=>{
+            let option=document.createElement("option")
+            option.text=item.candidate;
+            select.add(option);
+            option.value=item.candidate;
 
+        })
+        // console.log(typeof(arrayvaccine));
+        select.addEventListener("change",function(){
+            // console.log(this.value);
+            // if(this.value==)
+            arrayvaccine.forEach(item=>{
+                console.log(item);
+               if(this.value==item.candidate){
+                   container.innerHTML=`<table><tr><th>Candidate</th><th>Mechanism</th><th>Sponsors</th><th>Trial Phase</th></tr>
+                   <tr>       <td>${item.candidate}</td>  <td>${item.mechanism}</td>  <td>${item.sponsors}</td>  <td>${item.trialPhase}</td>  </tr>
+                   
+                   
+                   </table>`
+               }
+            })
+})
+    })
+}
+
+
+
+// Drop down function
+dropdownfunc();
 
