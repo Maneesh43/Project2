2da79cff742b16277928afa6ec43d2a82fbb877b
Maneesh43 <thouti.maneesh43@gmail.com>
2021-03-30 23:08:02 -0700

    minor changes

--- scripts/vaccineregister.js
+++ scripts/vaccineregister.js
@@ -287,4 +287,38 @@ document.querySelector('#signouthere').addEventListener('click',()=>{
             })
 
 
+            // PWA banner
+
+
+var deferredPrompt; 
+
+window.addEventListener('beforeinstallprompt', function (e) { 
+  deferredPrompt = e; 
+  showAddToHomeScreen(deferredPrompt);
+  console.log(deferredPrompt);
+ }); 
+
+ function showAddToHomeScreen() { 
+   var a2hsBtn = document.querySelector(".pwabanner"); 
+   console.log(a2hsBtn);
+   a2hsBtn.style.display = "block";
+  //  a2hsBtn.style.justifyContent="space-around"; 
+   a2hsBtn.addEventListener("click", addToHomeScreen); 
+  } 
+
+  function addToHomeScreen() { 
+    var a2hsBtn = document.querySelector(".pwabanner");  
+    a2hsBtn.style.display = 'none'; // Show the prompt 
+    deferredPrompt.prompt(); // Wait for the user to respond to the prompt 
+    deferredPrompt.userChoice .then(function(choiceResult){ 
+      if (choiceResult.outcome === 'accepted') { 
+        console.log('User accepted the A2HS prompt'); 
+      } 
+      else 
+      { 
+        console.log('User dismissed the A2HS prompt'); 
+      } 
+      deferredPrompt = null; 
+    }); } 
+
             
